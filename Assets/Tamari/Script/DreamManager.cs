0311097e0fae81d6813ccad496714ef9991b10f8
MizutamariShutarou <shutaro0415@outlook.jp>
2023-12-20 15:32:43 +0900

    エラー修正

--- Assets/Tamari/Script/DreamManager.cs
+++ Assets/Tamari/Script/DreamManager.cs
@@ -25,7 +25,7 @@ public class DreamManager : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameManager.Instance.TimeManager.DecreaseTimere();
+            GameManager.Instance.TimeManager.ReduceTimere();
             SceneStateManager.instance.LoadScene(SceneType.RealWorld);
             _isChanged = true;
         }
